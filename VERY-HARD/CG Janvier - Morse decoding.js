bd56a3cbce5e0a13ad8d3cf99b2969d5f4d59f03
Anc813 <anc813@gmail.com>
2015-01-22 04:39:40 +0200

    Update CG Janvier - Morse decoding.js

--- VERY-HARD/CG Janvier - Morse decoding.js
+++ VERY-HARD/CG Janvier - Morse decoding.js
@@ -24,6 +24,32 @@ var chars = {
     O: "---",
     P: ".--.",
     Q: "--.-",
+    R: ".-.",a/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var L = readline();
+var N = parseInt(readline());
+
+var chars = {
+    A: ".-",
+    B: "-...",
+    C: "-.-.",
+    D: "-..",
+    E: ".",
+    F: "..-.",
+    G: "--.",
+    H: "....",
+    I: "..",
+    J: ".---",
+    K: "-.-",
+    L: ".-..",
+    M: "--",
+    N: "-.",
+    O: "---",
+    P: ".--.",
+    Q: "--.-",
     R: ".-.",
     S: "...",
     T: "-",
@@ -32,6 +58,51 @@ var chars = {
     W: ".--",
     X: "-..-",
     Y: "-.--",
+    Z: "--..",    
+}
+
+function encodeWord(word) {
+    return word.split('').map(function(e){return chars[e]}).join('');
+}
+
+var words = [];
+for (var i = 0; i < N; i++) {
+    var W = readline();
+    var word = encodeWord(W);
+    // filter useless words
+    if (~L.indexOf(word)) words.push(word);
+}
+
+var maxwordlength = words.reduce(function(a,b){return (b.length>a)?b.length:a},0);
+
+var dict = {};
+
+function analyzePart(s) {
+    if (s in dict) return dict[s];
+
+    var sum = 0;
+    var short = s.slice(0,maxwordlength);
+    
+    for(var i = 0; i < words.length; i++) {
+        var word = words[i];
+
+        if (word.length > s.length) continue;
+
+        if (short.indexOf(word) === 0) {
+            // analyze rest
+            var newWord = s.slice(word.length);
+            
+            sum += (newWord) ? analyzePart(newWord) : 1;
+        }
+    }
+    
+    dict[s] = sum;
+    return sum;
+}
+// Write an action using print()
+// To debug: printErr('Debug messages...');
+
+print(analyzePart(L));
     Z: "--..",    
 }
 
